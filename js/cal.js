03f3de93c754ac928734118f99bf340201d741cc
Blake Hause <blakehause@Blakes-MBP.fc.ca>
2018-06-13 14:00:54 -0400

    added selectors and listeners

--- js/cal.js
+++ js/cal.js
@@ -3,18 +3,29 @@
 	console.log("JS file loaded")
 
     //vars
+    var num1 = document.querySelector("#input1");
+    var num2 = document.querySelector("#input2");
+    var equals = document.querySelector("#trigger");
+    var outcome = document.querySelector("#value");
+    // console.log(num1);
+    // console.log(num2);
+    // console.log(equals);
+    // console.log(outcome);
 
 
 
 
 
     //functions
-
+    function addStuff() {
+    	console.log("from addStuff");
+    }
 
 
 
 
     //listeners
+    equals.addEventListener("click", addStuff, false);
 
 
 })();
